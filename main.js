2cd748acea256e2d5e0087e175906766c30079ac
Nemesis <zecaivano@gmail.com>
2022-12-11 00:48:13 -0300

    Dia 03/07 - Um belo upgrade com local Storage

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-const nome = document.querySelector('#name')
-const nascimento = document.querySelector('#birth-date')
-const submit = document.querySelector(".salvar")
+const form = document.querySelector(".form")
+const lista = document.getElementById("lista")
+const pessoas = JSON.parse(localStorage.getItem("pessoas")) || []
 
-function consolar() {
-    console.log(nome.value);
-    console.log(nascimento.value);
-}
-
-submit.addEventListener('click', (evento) => {
-    evento.preventDefault();
-    consolar();
+pessoas.forEach ((elemento) => {
+    criaElemento(elemento)
 })
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+     
+    const nome = evento.target.elements['name']
+    const nascimento = evento.target.elements['birth-date']
+    const pessoa = {
+        "nome": nome.value,
+        "nascimento": nascimento.value
+    }
+
+    criaElemento(pessoa)
+    
+    pessoas.push(pessoa)
+    localStorage.setItem("pessoas", JSON.stringify(pessoas))
+
+    nome.value = ''
+    nascimento.value = ''
+})
+
+function criaElemento(linha) {
+    const novaLinha = document.createElement('li')
+    novaLinha.classList.add("list-group-item")
+
+    const celulaNome = document.createElement('strong')
+    celulaNome.innerHTML = linha.nome
+    novaLinha.appendChild(celulaNome)
+    novaLinha.innerHTML += linha.nascimento
+
+    lista.appendChild(novaLinha)
+}
